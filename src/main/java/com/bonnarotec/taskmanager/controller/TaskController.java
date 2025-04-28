8f403a535bb70322870c8a4f93f1da6416e7f966
LucasBonna <lucasbonnafavaro@gmail.com>
2025-04-28 16:50:11 -0300

    remove security

--- src/main/java/com/bonnarotec/taskmanager/controller/TaskController.java
+++ src/main/java/com/bonnarotec/taskmanager/controller/TaskController.java
@@ -1,10 +1,16 @@
 package com.bonnarotec.taskmanager.controller;
 
+import com.bonnarotec.taskmanager.dto.MessageReturn;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping("/tasks")
 public class TaskController {
-
+    @GetMapping()
+    public ResponseEntity<MessageReturn> getMessage() {
+        return ResponseEntity.ok(new MessageReturn("OK"));
+    }
 }
---
+++ src/main/java/com/bonnarotec/taskmanager/dto/MessageReturn.java
@@ -0,0 +1,6 @@
+package com.bonnarotec.taskmanager.dto;
+
+public record MessageReturn(
+        String Message
+) {
+}
